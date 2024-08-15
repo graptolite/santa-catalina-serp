ca7dcad832197334a5d428122f8e6424abb383ca
graptolite <109924401+graptolite@users.noreply.github.com>
2024-08-15 14:28:45 +0000

    Track datasets.

---
+++ DATASETS/EBSD/ATTRIB.md
@@ -0,0 +1,1 @@
+Data collected by Brandon Fish.
---
+++ DATASETS/SERP/sample_serp.py
@@ -0,0 +1,19 @@
+# Declare the observed degree of serpentinization found for samples where thin sections were present.
+# P: partially serpentinized
+# H: heavily serpentinized
+# V: vein sample (not serpentinized mantle)
+serp = {"07A":"P",
+        "M04":"P",
+        "M08B":"P",
+        "M01":"H",
+        "M02":"H",
+        "M07B":"H",
+        "06C":"H",
+        "M01-vein":"V"}
+# Declare the colors to assign each serpentinization level.
+# P: dark green
+# H: purple
+# V: light blue
+cmap = {"P":"#003f00",
+        "H":"#800080",
+        "V":"#00f4ef"}
